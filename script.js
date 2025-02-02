762f4d02195a8d523b27d8c5302384132b01d1c7
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-02-03 00:06:10 +0600

    js if-else problem

---
+++ script.js
@@ -0,0 +1,20 @@
+let number = -39;
+
+if (number > 0){
+    console.log ("The number is positive");
+}
+
+else{
+    console.log ("The number is negative");
+}
+
+
+let number2 = 74569;
+
+if (number2%2 == 0){
+    console.log ("This is an even number");
+}
+
+else{
+    console.log("This is an odd number");
+}
